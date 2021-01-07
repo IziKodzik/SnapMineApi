401909ce89ce78c7c83554a2ee49284f709df95b
IziKodzik <itiguuy@gmail.com>
2021-01-07 07:05:53 +0100

    Base64 is the solution

--- src/main/java/com/snapmine/SnapMineApi/service/SecurityServiceImpl.java
+++ src/main/java/com/snapmine/SnapMineApi/service/SecurityServiceImpl.java
@@ -4,17 +4,16 @@ import com.snapmine.SnapMineApi.cryptor.AESCryptor;
 import com.snapmine.SnapMineApi.dao.ClientDao;
 import com.snapmine.SnapMineApi.dao.ClientDataAccessServicePostgres;
 import com.snapmine.SnapMineApi.model.Client;
-import com.snapmine.SnapMineApi.model.Role;
-import com.snapmine.SnapMineApi.model.SessionToken;
 import com.snapmine.SnapMineApi.model.dtos.request.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.Base64;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -22,10 +21,11 @@ import java.util.function.Function;
 public class SecurityServiceImpl
 	implements SecurityService{
 
-
+	private final BASE64Decoder base64Decoder = new BASE64Decoder();
+	private final BASE64Encoder base64Encoder = new BASE64Encoder();
 	private final ClientDao DB;
 	private AESCryptor aesCryptor;
-
+	private Charset CHARSET = StandardCharsets.UTF_8; // ISO-8859-1 vs. UTF-8
 	@Autowired
 	public SecurityServiceImpl(ClientDataAccessServicePostgres db,
 							   AESCryptor aesCryptor, Function<String,String> secret) {
@@ -52,11 +52,17 @@ public class SecurityServiceImpl
 	@Override
 	public Optional<String> authenticate() {
 
-		Optional<List<Role>> maybeRoles = this.DB.getRolesById(2);
-		List<Role> roleList = maybeRoles.orElse(new ArrayList<>());
-		SessionToken token = new SessionToken(roleList);
-		String test = new String(aesCryptor.encrypt(token.getToken().getBytes(StandardCharsets.UTF_8)));
-		System.out.println(new String(aesCryptor.decrypt(test.getBytes(StandardCharsets.UTF_8))));
+		String mess = "message";
+		String costam = base64Encoder.encode(aesCryptor.encrypt(mess.getBytes(CHARSET)));
+		System.out.println(costam);
+		byte[] decodedBytes = Base64.getDecoder().decode(costam);
+		decodedBytes = aesCryptor.decrypt(decodedBytes);
+		System.out.println(new String(decodedBytes));
+		//		Optional<List<Role>> maybeRoles = this.DB.getRolesById(2);
+//		List<Role> roleList = maybeRoles.orElse(new ArrayList<>());
+//		SessionToken token = new SessionToken(roleList);
+//		aesCryptor.encrypt(token.getToken().getBytes(StandardCharsets.UTF_8));
+//		(new String(aesCryptor.decrypt(.getBytes(StandardCharsets.UTF_8))));
 		return Optional.of("XD");
 
 	}
